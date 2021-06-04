8ad57b59ef9d05c877fed64b09a3aa65b789fbfe
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-06-04 18:38:31 +0530

    Added : Creating Tic Tac Toe Game board

--- src/com/TicTacToeGame.java
+++ src/com/TicTacToeGame.java
@@ -1,7 +1,17 @@
 package com;
 
 public class TicTacToeGame {
+	static int board[][] = new int[3][3];
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
+		createBoard();
+		System.out.println("Board is created");
+	}
+	public static void createBoard() {
+		for(int i=0;i<board.length;i++) {
+			for(int j =0;j<board.length;j++) {
+				board[i][j]=' ';
+			}
+		}
 	}
 }
